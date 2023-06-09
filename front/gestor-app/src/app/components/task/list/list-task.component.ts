32cab8447a572a8eef7c04ca393f3afc4ad83d6b
guerreroaanathalia@gmail.com <guerreroaanathalia@gmail.com>
2023-06-09 15:38:49 -0500

    listar casi ready

--- front/gestor-app/src/app/components/task/list/list-task.component.ts
+++ front/gestor-app/src/app/components/task/list/list-task.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { TaskService } from '../task.service';
 import { Task } from '../task.model';
 
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-list-task',
   templateUrl: './list-task.component.html',
 })
 
-export class ListTaskComponent implements OnInit{
+export class ListTaskComponent implements OnInit,AfterViewInit{
 
   public tasks: Task[] = [];
 
@@ -16,6 +17,13 @@ export class ListTaskComponent implements OnInit{
 
   ngOnInit() {
     this.getAll();
+  }
+
+  ngAfterViewInit() {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
   }
   getAll() {
     this.service.getAll().subscribe((tasks)=>{
--- front/gestor-app/src/app/components/task/task.module.ts
+++ front/gestor-app/src/app/components/task/task.module.ts
@@ -1,23 +1,36 @@
-import { NgModule } from "@angular/core";
+import { Component, NgModule } from "@angular/core";
 import { ListTaskComponent } from './list/list-task.component';
 import { RouterModule, Routes } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms';
+import { CreateComponent } from './create/create.component';
+import { SharedModule } from "../shared/shared.module";
+import { FormComponent } from "./form/form.component";
+
 
 const routes: Routes =[
     {
-        path:'',
+        path:'list',
         component:ListTaskComponent,
+    },
+    {
+        path:'new',
+        component:CreateComponent,
     }
 ]
 
 @NgModule({
     imports:[
         RouterModule.forChild(routes),
+        FormsModule,
+        SharedModule,
         CommonModule
     ],
-    exports:[RouterModule,],
+    exports:[RouterModule],
     declarations:[
         ListTaskComponent,
+        CreateComponent,
+        FormComponent
     ],
 })
 
